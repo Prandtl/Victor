9ef2ef9871b49955c4e3182e8b01ea8acb6ccdf2
Prandtl <vasiliy.uchanev@prandtl.co>
2018-12-25 11:55:31 +0000

    added streamer & task worker for sounds

---
+++ task_worker_sounds.py
@@ -0,0 +1,25 @@
+import time
+import sys
+import zmq
+import random
+import vlc
+import urllib.parse as urlparse
+
+def consumer():
+    consumer_id = random.randrange(1, 10005)
+    context = zmq.Context()
+    reciever = context.socket(zmq.PULL)
+    reciever.connect("tcp://127.0.0.1:5566")
+    while True:
+        try:
+            work = reciever.recv_json()
+            path = work["message"]
+            p = vlc.MediaPlayer("./" + path)
+            p.play()
+        except Exception as e:
+            print("SOUNDS", e)
+            context = zmq.Context()
+            reciever = context.socket(zmq.PULL)
+            reciever.connect("tcp://127.0.0.1:5566")
+
+consumer()
